fbed662e1f63071d11038adb47f07ebb5130632b
Whoroot <whoroot@users.noreply.github.com>
2017-03-27 17:48:54 +0200

    Update gages_online.php

--- application/config/gages_online.php
+++ application/config/gages_online.php
@@ -7,83 +7,50 @@
 */
 
 /*** Page 1 ***/
-// SENSUEL //
+// NIVEAU 1 //
 $GLOBALS['liste_1'] = array(
-1=>"Envoyer une photo sensuellement provocatrice du torse/poitrine (on doit deviner que la partie est nue, même si on ne le voit pas entièrement et directement)",
-2=>"Envoyer à l'autre une phrase décrivant une envie sensuelle du moment",
-3=>"Envoyer un enregistrement audio d'un soupir sensuel",
-4=>"Envoyer une photo sensuellement provocatrice des fesses",
-5=>"Envoyer une photo sensuellement provocatrice du sexe (dissimulé par une main, un tissu, ...)",
-6=>"Envoyer une photo sensuellement provocatrice de votre choix (chinée sur le Net autorisée)",
-7=>"Envoyer une photo sensuellement provocatrice de votre choix, mais de VOUS (où vous êtes le modèle)",
-8=>"A la manière de action/vérité, répondre à une question de l'autre joueur, en toute franchise",
-9=>"Avouer à l'autre 1 envie/désir non réalisé, mais que vous espérez bien mettre en pratique un jour",
-10=>"Avouer à l'autre 1 envie/désir déjà réalisé, et qu'il vous délecterait de mettre en pratique avec elle/lui",
-11=>"Avouer à l'autre un de vos péché-mignons que vous vous délecter d'offrir à l'autre",
-12=>"Avouer à l'autre un de vos péché-mignons que vous vous délecter de recevoir de l'autre",
-13=>"Décrire à l'autre une scène/acte coquin(e) que vous imagineriez bien partager avec elle/lui",
-14=>"Décrire à l'autre une scène/acte HOT que vous imagineriez bien partager avec elle/lui",
-15=>"Envoyer une phrase écrite provoquante et 'hot' susceptible d'émousser l'excitation de l'autre"
-
+1=>"Lorem ipsum 1 dolor sit amet, consectetur adipisicing elit",
+2=>"Lorem ipsum 2 dolor sit amet, consectetur adipisicing elit",
+3=>"Lorem ipsum 3 dolor sit amet, consectetur adipisicing elit",
+4=>"Lorem ipsum 4 dolor sit amet, consectetur adipisicing elit",
 );
 
 $GLOBALS['total_gage_1'] = count($GLOBALS['liste_1']);
 
 
 /*** Page 2 ***/
-// COQUIN //
+// NIVEAU 2 //
 
 $GLOBALS['liste_2'] = array(
-1=>"Envoyer une vidéo montrant une partie intime de vous (10 sec)",
-2=>"Envoyer un photo incitatrice/provocatrice d'une partie intime non dissimulée",
-3=>"Envoyer un enregistrement audio d'une phrase coquine comportant 4 mots au choix de l'autre joueur",
-4=>"Envoyer un enregistrement audio d'un gémissement sensuel (du genre 'hummm')",
-5=>"Envoyer une photo coquine et provocatrice (d'un acte sexuel, d'une position,...) de votre choix (chinée sur le Net autorisée)",
-6=>"Envoyer une photo coquine et provocatrice (par votre regard, votre expression de visage, de corps,...) de VOUS",
-7=>"Envoyer une photo de votre sexe non dissimulé (sexe en main (H) / doigt dans sexe (F))",
-8=>"Envoyer une vidéo de vous en train de vous mordiller/lécher les lèvres (5-10 sec)",
-9=>"Faire un gage de VOTRE choix parmi les exemples illustrés online (onglet jaune)",
-10=>"Faire un strip tease en cam (jusqu'à être en sous-vêtement). Vidéo envoyée acceptée",
-11=>"Faire une photo en lingerie sexy (porte-jartelle, bustier, dentelle,...). Homme : boxer/calçon moulant en jouant sur l'angle et/ou la mise en scène"
-
+1=>"Lorem ipsum 1 dolor sit amet, consectetur adipisicing elit",
+2=>"Lorem ipsum 2 dolor sit amet, consectetur adipisicing elit",
+3=>"Lorem ipsum 3 dolor sit amet, consectetur adipisicing elit",
+4=>"Lorem ipsum 4 dolor sit amet, consectetur adipisicing elit",
 );
 
 $GLOBALS['total_gage_2'] = count($GLOBALS['liste_2']);
 
 /*** page 3 ***/
-// HOT //
+// NIVEAU 3 //
 
 $GLOBALS['liste_3'] = array(
-1=>"Se caresser le sexe en cam > 15 sec (vidéo envoyée acceptée)",
-2=>"Se caresser le torse/poitrine en cam > 15 sec (vidéo envoyée acceptée)",
-3=>"Envoyer un enregistrement audio cru/provocant (J'ai 1 envie folle de ta queue/chatte, ...)",
-4=>"Envoyer un enregistrement audio d'un râle de plaisir (simulant l'orgasme)",
-5=>"Faire un gage de VOTRE choix parmi les exemples illustrés online (onglet jaune)",
-6=>"Faire un gage au choix de l'autre joueur, parmi les exemples illustrés online (onglet jaune)",
-7=>"Exécuter en cam une requête au choix de l'autre",
-8=>"Exécuter en photo une requête au choix de l'autre",
-9=>"Se provoquer un orgasme en cam sous le regard de l'autre (qui peut, à son choix, participer aussi)",
+1=>"Lorem ipsum 1 dolor sit amet, consectetur adipisicing elit",
+2=>"Lorem ipsum 2 dolor sit amet, consectetur adipisicing elit",
+3=>"Lorem ipsum 3 dolor sit amet, consectetur adipisicing elit",
+4=>"Lorem ipsum 4 dolor sit amet, consectetur adipisicing elit",
 );
 
 $GLOBALS['total_gage_3'] = count($GLOBALS['liste_3']);
 
 
 /*** page 8 ***/
-### MEDLEY ###
+### NIVEAU 4 ###
 
 $GLOBALS['liste_8'] = array(
-1=> "Prendre une photo coquine de vous dans un lieu extérieur (autre que chez soi)",
-2=> "Decrire une scène coquine (hot ou sauvage ou sensuel peu importe) que vous aimeriez faire avec elle/lui dans un lieu public",
-3=> "Envoyer un enregistrement audio d'une envie coquine alors que vous n'êtes pas seul(e)",
-4=> "Avouer à l'autre 1 envie/désir NON réalisé, mais que vous espérez bien mettre en pratique un jour",
-5=> "Avouer à l'autre 1 envie/désir DEJA réalisé en extérieur, et qu'il vous délecterait de mettre en pratique avec elle/lui",
-6=> "Décrire à l'autre une scène/acte coquin(e) que vous imagineriez bien partager avec elle/lui",
-7=> "Faire un gage de VOTRE choix parmi les exemples illustrés online (onglet jaune)",
-8=> "A la manière de action/vérité, répondre à une question de l'autre joueur, en toute franchise",
-9=> "Prendre une photo laissant apparaître clairement un sein/pectoraux, tout en étant dans un lieu publique (on doit voir tout ou partie, au moins, du mamelon",
-10=>"Prendre une photo laissant apparaître votre sexe, tout en étant dans un lieu publique (tout ou partie de toison acceptée, si il y a)",
-11=>"Prendre une photo/vidéo de vos lèvres et/ou regard sensuel (dans votre contexte actuel, c'est à dire au bureau, dans la rue, etc)"
-
+1=>"Lorem ipsum 1 dolor sit amet, consectetur adipisicing elit",
+2=>"Lorem ipsum 2 dolor sit amet, consectetur adipisicing elit",
+3=>"Lorem ipsum 3 dolor sit amet, consectetur adipisicing elit",
+4=>"Lorem ipsum 4 dolor sit amet, consectetur adipisicing elit",
 );
 $GLOBALS['total_gage_8'] = count($GLOBALS['liste_8']);
 
